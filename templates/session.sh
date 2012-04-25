83744f77a7d70f94cf1802306b57e1cc60c0df13
Jim Myhrberg <contact@jimeh.me>
2012-04-25 22:54:17 +0100

    Set commented out example of root path to new window/session name

--- templates/session.sh
+++ templates/session.sh
@@ -1,6 +1,6 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-#session_root "~/Projects/example"
+#session_root "~/Projects/{{SESSION_NAME}}"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
--- templates/window.sh
+++ templates/window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-#window_root "~/Projects/example"
+#window_root "~/Projects/{{WINDOW_NAME}}"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
