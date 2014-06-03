94b9559bfad2d1ffcaa6681845a0115ded457845
Jim Myhrberg <contact@jimeh.me>
2014-06-03 20:31:19 +0100

    Use full tmux command names rather than aliases

--- test/libexec/tmuxifier-tmux.test.sh
+++ test/libexec/tmuxifier-tmux.test.sh
@@ -9,8 +9,8 @@ source "${root}/lib/util.sh"
 # Setup.
 libexec="${root}/libexec"
 export TMUXIFIER_TMUX_OPTS="-L tmuxifier-tests"
-tmux $TMUXIFIER_TMUX_OPTS new -d -s foobar
-tmux $TMUXIFIER_TMUX_OPTS new -d -s dude
+tmux $TMUXIFIER_TMUX_OPTS new-session -d -s foobar
+tmux $TMUXIFIER_TMUX_OPTS new-session -d -s dude
 
 # Passes all arguments to Tmux.
 assert "${libexec}/tmuxifier-tmux list-sessions -F \"- #{session_name}\"" \
