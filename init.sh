jimeh/tmuxifier

commit 51ebf880dcfd28ba773c91f44c13f87007cb4fce

Only add $TMUXIFIER/bin to $PATH if it is not in $PATH already

init.sh
@@ -6,7 +6,9 @@ else
 fi
 
 # Add `bin` directroy to `$PATH`.
-export PATH="$TMUXIFIER/bin:$PATH"
+if [[ ":$PATH:" != *":$TMUXIFIER/bin:"* ]]; then
+  export PATH="$TMUXIFIER/bin:$PATH"
+fi
 
 # If `tmuxifier` is available, and `$TMUXIFIER_NO_COMPLETE` is not set, then
 # load tmuxifier shell completion.

commit fbfbba4b08d0834229eca1450b9adf04c9b75ae5

Fix bashism
51ebf880 introduced a bashism, resulting in errors in POSIX
environments.

init.sh
@@ -6,7 +6,7 @@ else
 fi
 
 # Add `bin` directroy to `$PATH`.
-if [[ ":$PATH:" != *":$TMUXIFIER/bin:"* ]]; then
+if [ $(expr ":$PATH:" : ".*:$TMUXIFIER/bin:.*") -eq 0 ]; then
   export PATH="$TMUXIFIER/bin:$PATH"
 fi
 